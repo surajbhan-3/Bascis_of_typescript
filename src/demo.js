84c778567e302e25c38aa97f3dd1654f245d1a56
surajbhan-3 <surajbhan2boaz@gmail.com>
2023-05-19 23:58:08 +0530

    ts

--- src/demo.js
+++ src/demo.js
@@ -1,1 +1,15 @@
 console.log("hello world");
+var a = "Good vibes";
+a = "Better vibes";
+//  a=5576   // throwing error because it's explicitly defined that a  is valriable having a datatype of  string  not a number so this will through error 
+var b = 343;
+//  b="old memories"// throwing error because  b is a variable storing number datatypes  
+var c = true;
+c = false; // correct way
+//    c="hello"// will throw error
+// null , undefined
+// Here we can use union type operator  to tell that  variable  ca n be null or can be string
+var d = null;
+d = "Great things ahead";
+d = null;
+d = "Great things ";
--- src/demo.ts
+++ src/demo.ts
@@ -4,9 +4,27 @@ console.log("hello world")
 let a: string="Good vibes"
 
  a="Better vibes";
- a=5576   // throwing error because it's explicitly defined that a  is valriable having a datatype of  string  not a number so this will through error 
+//  a=5576   // throwing error because it's explicitly defined that a  is valriable having a datatype of  string  not a number so this will through error 
 
 let  b: number=343;
- b="old memories"// throwing error because  b is a variable storing number datatypes  
+//  b="old memories"// throwing error because  b is a variable storing number datatypes  
 
+
+ let c: boolean = true;
+
+   c=false; // correct way
+//    c="hello"// will throw error
+
+
+// null , undefined
+
+// Here we can use union type operator  to tell that  variable  ca n be null or can be string
+   
+ let d: null | string=null;
  
+  d= "Great things ahead";
+  d= null;
+  d= "Great things "
+
+
+
